52467b0d0a452925fcb348c12958a5da285e467a
shahroz002 <shahrozshkl@gmail.com>
2025-04-28 13:51:39 +0500

    frontend integration done
    Frontend integrated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from agents.routes import router as agent_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(
     title="AI Analyst Agents",
@@ -7,5 +9,13 @@ app = FastAPI(
     version="1.0.0"
 )
 
+templates = Jinja2Templates(directory="templates")
+
+
+
 # Include the agent router under the /api path
 app.include_router(agent_router, prefix="/api")
+# Route to render HTML
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
