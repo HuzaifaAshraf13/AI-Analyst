1003b717503c1f5d8ebf6d1414e5250fd509f995
shahroz002 <shahrozshkl@gmail.com>
2025-04-30 11:59:16 +0500

    Set up the environment

--- entry.py
+++ entry.py
@@ -1,4 +1,17 @@
 import uvicorn
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.0", port=8000, reload=True)
+    # Check environment variable
+    environment = os.getenv("ENV", "local")  # Default is "local" if ENV not set
+
+    if environment == "production":
+        # Render.com or production environment
+        port = int(os.environ.get("PORT", 8000))
+        uvicorn.run("main:app", host="0.0.0.0", port=port)
+    else:
+        # Local development
+        uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
